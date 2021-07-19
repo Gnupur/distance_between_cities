2b3b3d3d245608adb78009126151b085ac450900
Gnupur <nupursharad123@gmail.com>
2021-07-19 21:20:12 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Introduction to Google Maps Javascript API</title>
+        <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+        <meta charset="utf-8">
+        <style>
+            html, body{
+                height:100%;   
+            }
+            #map{
+                height:60%;   
+            }
+        </style>
+    </head>
+    
+    <body>
+        <div id="map"></div>
+        
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCwJ2Vepe9L2Miuh7QH87SR_RItIXHlX6Q"></script>
+        <script>
+            
+            
